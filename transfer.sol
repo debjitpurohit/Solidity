6466c6b3ebd64ed8d4003caeb7a1a233113e9994
DEBJIT PUROHIT <92798917+debjitpurohit@users.noreply.github.com>
2023-08-05 03:52:07 +0530

    Update transfer.sol

--- transfer.sol
+++ transfer.sol
@@ -1,1 +1,18 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.7.0;
 
+contract transfer{
+    address public owner;
+    event transactions(address to,uint amount);//EVENT for ether.js
+
+    constructor(){
+        owner=msg.sender;
+    }
+    function callowner() public view returns(address){
+        return owner;
+    }
+    function _transfer(address payable _to) public payable {
+        _to.transfer(msg.value);
+        emit transactions(_to, msg.value);//save to ether.js
+    }
+}
